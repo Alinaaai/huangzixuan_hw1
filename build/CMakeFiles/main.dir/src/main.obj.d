bb34406d1022ee754c8a0dc34f062cbb65601a4c
Alina <3534066576@qq.com>
2024-05-13 19:20:18 +0800

    first commit

---
+++ build/CMakeFiles/main.dir/src/main.obj.d
@@ -0,0 +1,16 @@
+CMakeFiles/main.dir/src/main.obj: \
+ C:\Users\35340\Desktop\x-lab\my_project\huangzixuan_hw1\src\main.c \
+ C:/Users/35340/Downloads/x86_64-8.1.0-release-win32-sjlj-rt_v6-rev0/mingw64/x86_64-w64-mingw32/include/stdio.h \
+ C:/Users/35340/Downloads/x86_64-8.1.0-release-win32-sjlj-rt_v6-rev0/mingw64/x86_64-w64-mingw32/include/crtdefs.h \
+ C:/Users/35340/Downloads/x86_64-8.1.0-release-win32-sjlj-rt_v6-rev0/mingw64/x86_64-w64-mingw32/include/_mingw.h \
+ C:/Users/35340/Downloads/x86_64-8.1.0-release-win32-sjlj-rt_v6-rev0/mingw64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/Users/35340/Downloads/x86_64-8.1.0-release-win32-sjlj-rt_v6-rev0/mingw64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/Users/35340/Downloads/x86_64-8.1.0-release-win32-sjlj-rt_v6-rev0/mingw64/x86_64-w64-mingw32/include/vadefs.h \
+ C:/Users/35340/Downloads/x86_64-8.1.0-release-win32-sjlj-rt_v6-rev0/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_directx.h \
+ C:/Users/35340/Downloads/x86_64-8.1.0-release-win32-sjlj-rt_v6-rev0/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/Users/35340/Downloads/x86_64-8.1.0-release-win32-sjlj-rt_v6-rev0/mingw64/x86_64-w64-mingw32/include/_mingw_print_push.h \
+ C:/Users/35340/Downloads/x86_64-8.1.0-release-win32-sjlj-rt_v6-rev0/mingw64/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/Users/35340/Downloads/x86_64-8.1.0-release-win32-sjlj-rt_v6-rev0/mingw64/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/Users/35340/Downloads/x86_64-8.1.0-release-win32-sjlj-rt_v6-rev0/mingw64/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ C:/Users/35340/Downloads/x86_64-8.1.0-release-win32-sjlj-rt_v6-rev0/mingw64/x86_64-w64-mingw32/include/_mingw_print_pop.h \
+ C:/Users/35340/Desktop/x-lab/my_project/huangzixuan_hw1/inc/algebra.h
--- src/algebra.c
+++ src/algebra.c
@@ -12,37 +12,107 @@ Matrix create_matrix(int row, int col)
 
 Matrix add_matrix(Matrix a, Matrix b)
 {
-    // ToDo
-    return create_matrix(0, 0);
+    if ((a.rows != b.rows) || (a.cols != b.cols))
+    {
+        printf("Error: Matrix a and b must have the same rows and cols.\n");
+        return create_matrix(0, 0);
+    }
+    Matrix result = create_matrix(a.rows, a.cols);
+    for (int i = 0; i < a.rows; i++)
+    {
+        for (int j = 0; j < a.cols; j++)
+        {
+            result.data[i][j] = a.data[i][j] + b.data[i][j];
+        }
+    }
+    return result;
 }
 
 Matrix sub_matrix(Matrix a, Matrix b)
 {
-    // ToDo
-    return create_matrix(0, 0);
+    if ((a.rows != b.rows) || (a.cols != b.cols))
+    {
+        printf("Error: Matrix a and b must have the same rows and cols.\n");
+        return create_matrix(0, 0);
+    }
+    Matrix result = create_matrix(a.rows, a.cols);
+    for (int i = 0; i < a.rows; i++)
+    {
+        for (int j = 0; j < a.cols; j++)
+        {
+            result.data[i][j] = a.data[i][j] - b.data[i][j];
+        }
+    }
+    return result;
 }
 
 Matrix mul_matrix(Matrix a, Matrix b)
 {
-    // ToDo
-    return create_matrix(0, 0);
+    if (a.cols != b.rows)
+    {
+        printf("Error: The number of cols of matrix a must be equal to the number of rows of matrix b.\n");
+        return create_matrix(0, 0);
+    }
+    Matrix result = create_matrix(a.rows, b.cols);
+    for (int i = 0; i < a.rows; i++)
+    {
+        for (int j = 0; j < b.cols; j++)
+        {
+            for (int k = 0; k < a.cols; k++)
+            {
+                result.data[i][j] += a.data[i][k] * b.data[k][j];
+            }
+        }
+    }
+    return result;
 }
 
 Matrix scale_matrix(Matrix a, double k)
 {
-    // ToDo
-    return create_matrix(0, 0);
+    Matrix result = create_matrix(a.rows,a.cols);
+    for (int i = 0; i < a.rows; i++)
+    {
+        for (int j = 0; j < a.cols; j++)
+        {
+            result.data[i][j] = a.data[i][j] * k;
+        }
+    }
+    return result;
 }
 
 Matrix transpose_matrix(Matrix a)
 {
-    // ToDo
-    return create_matrix(0, 0);
+    Matrix result = create_matrix(a.cols,a.rows);
+    for (int i = 0; i < a.cols; i++)
+    {
+        for (int j = 0; j < a.rows; i++)
+        {
+            result.data[i][j] = a.data[j][i];
+        }
+    }
+    return result;
 }
 
 double det_matrix(Matrix a)
 {
-    // ToDo
+    int i, j, det = 0;
+    if (a.rows != a.cols)
+    {
+        printf("Error: The matrix must be a square matrix.\n");
+        return 0;
+    }
+    int n = a.rows;
+    Matrix result = create_matrix(a.cols,a.rows);
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            if (n == 2)
+            {
+                return (a.data[0][0]*a.data[1][1]-a.data[0][1]*a.data[1][0]);
+            }
+        }
+    }
     return 0;
 }
 
@@ -60,8 +130,17 @@ int rank_matrix(Matrix a)
 
 double trace_matrix(Matrix a)
 {
-    // ToDo
-    return 0;
+    double sum = 0;
+    if(a.rows != a.cols)
+    {
+        printf("Error: The matrix must be a square matrix.\n");
+        return 0;
+    }
+    for (int i = 0; i < a.rows; i++)
+    {
+        sum += a.data[i][i];
+    }
+    return sum;
 }
 
 void print_matrix(Matrix a)
